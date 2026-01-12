9e286d18ca368223c012d343cfa3ec8af01899c2
twinkle0tech <muskantamang925@gmail.com>
2026-01-12 21:17:52 +0530

    Start 100 Days of Python

---
+++ 1st_Day/app.py
@@ -0,0 +1,5 @@
+# BAND NAME GENERATOR
+print("Welcome To Band Name Generator")
+city=input("what's the name of the city you grew up in ?\n")
+pet=input("what's your pet name?\n")
+print("your band name could be " + city + " " + pet)
